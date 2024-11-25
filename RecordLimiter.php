2f3c8fe476bc83b9484612056e6fdbca8fc28b8d
Quazi Irfan <qui4001@med.cornell.edu>
2024-11-25 15:59:50 -0600

    Data export revoke/restore working

--- RecordLimiter.php
+++ RecordLimiter.php
@@ -21,8 +21,8 @@ class RecordLimiter extends \ExternalModules\AbstractExternalModule
     public function __construct(){
         parent::__construct();
 
-        $em_dir = $this->getModuleDirectoryName();
-        $em_prefix = substr($em_dir, 0, strrpos($em_dir, '_'));
+        $em_dir = $this->getModuleDirectoryName(); // record_limiter_v1.0.0
+        $em_prefix = substr($em_dir, 0, strrpos($em_dir, '_')); // record_limiter
         $em_id_query = $this->query("select external_module_id from redcap_external_modules where directory_prefix = ?", [$em_prefix]);
         $this->em_id = $em_id_query->fetch_assoc()['external_module_id'];
     }
@@ -52,11 +52,9 @@ class RecordLimiter extends \ExternalModules\AbstractExternalModule
         } else
             $record_limit = (int)$record_limit;
         
-        $user_info = $this->getUser(); 
+        $user_info = $this->getUser();
 
         if(!$user_info->isSuperUser()){
-            $current_user = $user_info->getUsername();
-            
             // find record count of this project
             $project_query = $this->query("SELECT project_id, status, record_count
                                             FROM redcap_projects 
@@ -71,25 +69,42 @@ class RecordLimiter extends \ExternalModules\AbstractExternalModule
                                                     username not in (select username from redcap_user_information where super_user = 1)", [$project_id]);
             $project = $this->getProject();
 
+            $already_active = $this->query("SELECT count(*) count from redcap_external_modules_log where project_id = ?", [$project_id]);
+            $already_active = $already_active->fetch_assoc()['count'];
+
             if($project_result['status'] == 0 && $project_result['record_count'] >= $record_limit){
                 // revoke
-                $pre_inst_export = array();
 
                 while($row = $project_users_query->fetch_assoc()){
+                    // {
+                    //     "username": "test2",
+                    //     "rights": {
+                    //       "record_create": 1,
+                    //       "user_rights": 1,
+                    //       "data_export_instruments": "[form_1,1][inst_2,3]"
+                    //     }
+                    // }
+
+                    $bkup = array();
+                    $bkup += ['username' => $row['username']];
+                    $rights = array();
+                    $bkup += array('rights' => &$rights);
+
                     // Apply limit 1 > 0
-                    if($project->getRights($row['username'])['record_create'] == 1)
+                    if($project->getRights($row['username'])['record_create'] == 1){
                         $project->setRights($row['username'], ['record_create' => 0]);
+                        $rights += ['record_create' => (int)1];
+                    }
 
                     // 1 Full Access > 0 No Access 
                     if($project->getRights($row['username'])['user_rights'] == 1){
                         $project->setRights($row['username'], ['user_rights' => 0]);
-                        $this->log('user_rights');
-                        // we don't take away record_delete when restoring
-                        if($project->getRights($row['username'])['record_delete'] == 0){
-                            $project->setRights($row['username'], ['record_delete' => 1]);
-                        }
+                        $rights += ['user_rights' => (int)1];
+                        if($project->getRights($row['username'])['record_delete'] == 0)
+                            $project->setRights($row['username'], ['record_delete' => 1]);                        
                     }
 
+                    $rights += ['data_export_instruments' => $row['data_export_instruments']];
                     $pre_inst_export_pairs = explode('][', $row['data_export_instruments']);
                     $revoked_inst_export = '';
                     $temp = array();
@@ -99,85 +114,52 @@ class RecordLimiter extends \ExternalModules\AbstractExternalModule
                         $temp += [$kv[0] => (int)$kv[1]];
                         $revoked_inst_export .= '['. $kv[0] . ',' . 0 .']';
                     }
-                    $pre_inst_export += [$row['username'] => $temp];
 
-                    $this->query("UPDATE redcap_user_rights 
-                                set data_export_instruments = ? 
-                                where username = ? and project_id = ?", [$revoked_inst_export, $row['username'], $project_id]);
-                    
+                    if($already_active == 0){
+                        $this->query("UPDATE redcap_user_rights 
+                                    set data_export_instruments = ? 
+                                    where username = ? and project_id = ?", 
+                                    [$revoked_inst_export, $row['username'], $project_id]);
+
+                        $this->log(json_encode($bkup));
+                    }
                 }
-                $this->log(json_encode($pre_inst_export));
-                echo '<div class="red">You have reached maximum number of records allowed in development status; (max allowed '.$record_limit.'). Please either move project to production or delete records to continue testing.</div>'; 
+                echo '<div class="red">
+                You have reached maximum number of records allowed in development status; (max allowed '.$record_limit.'). </br> 
+                Please either move project to production or delete records to continue testing. </br>
+                Reach out to user with user-rights access if recrod deletion feature is unavailable.
+                </div>'; 
             } else {
                 // restore
-                while($row = $project_users_query->fetch_assoc()){
-                    // Remove limit 0 > 1
-                    if($project->getRights($row['username'])['record_create'] == 0)
-                        $project->setRights($row['username'], ['record_create' => 1]);
+                // check if superuser has modified instrument information to ensure restoration is valid
+                // force restore as a option?
 
-                    // 0 No Access > 1 Full Access
-                    $em_user_rights_query = $this->query(
-                        "select *
-                        from redcap_external_modules_log
-                        where 
-                            external_module_id = ? and 
-                            project_id = ? and 
-                            message = 'user_rights' and
-                            ui_id not in (select ui_id from redcap_user_information where super_user = 1)",
-                        [$this->em_id, $project_id]
-                    );
-                    
-                    if($em_user_rights_query->num_rows == 1){
-                        $project->setRights($row['username'], ['user_rights' => 1]);
+                $old_rights = $this->query(
+                    "select log_id, message
+                    from redcap_external_modules_log
+                    where 
+                        external_module_id = ? and 
+                        project_id = ?",
+                    [$this->em_id, $project_id]
+                );
 
-                        $em_log_id = $em_user_rights_query->fetch_assoc()['log_id'];
+                while($row = $old_rights->fetch_assoc()){
+                    $message = json_decode($row['message'], True);
+
+                    $username = $message['username'];
+                    foreach($message['rights'] as $right_name => $old_value){
+                        $this->query("UPDATE redcap_user_rights set $right_name = ? where username = ? and project_id = ?",[$old_value, $username, $project_id]);
+
                         $this->query(
                             "delete
                             from redcap_external_modules_log
                             where log_id = ?",
-                            [$em_log_id]
+                            [$row['log_id']]
                         );
+
                     }
                 }
-
-                $em_inst_restore = $this->query(
-                    "select *
-                    from redcap_external_modules_log
-                    where 
-                        external_module_id = ? and 
-                        project_id = ? and 
-                        message != 'user_rights' and
-                        ui_id not in (select ui_id from redcap_user_information where super_user = 1)",
-                    [$this->em_id, $project_id]
-                );
-
-                if($em_inst_restore->num_rows == 1){
-                    $instruments = \REDCap::getInstrumentNames();
-                    $result = $em_inst_restore->fetch_assoc();
-                    $em_inst_restore_msg = json_decode($result['message'], True);
-                    foreach($em_inst_restore_msg as $user => $all_inst){
-                        $temp = '';
-                        foreach($all_inst as $inst_name => $inst_val){
-                            if(array_key_exists($inst_name, $instruments))
-                                $temp .= '['.$inst_name.','. $inst_val .']';
-                            else
-                                $temp .= '['.$inst_name.','. 1 .']'; // if a new instrument was added; we give them right to export data
-                        }
-                        
-                        $this->query("UPDATE redcap_user_rights
-                            set data_export_instruments = ? 
-                            where username = ? and project_id = ?", [$temp, $user, $project_id]);
-                    }
-                    
-                    $em_log_id = $result['log_id'];
-                    $this->query(
-                        "delete
-                        from redcap_external_modules_log
-                        where log_id = ?",
-                        [$em_log_id]
-                    );
-                }
-
+                
             } # revoke
         } # Superuser
     } # redcap_every_page_top
